3bb6f3bbd1a7f5f0d7030be7a69850117a6cfc21
heysarver <22250203+heysarver@users.noreply.github.com>
2024-11-15 20:09:17 -0500

    update cache to 1h

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
     TMDB_BASE_URL = "https://api.themoviedb.org/3"
     CACHE_TYPE = "SimpleCache"
-    CACHE_DEFAULT_TIMEOUT = 300
+    CACHE_DEFAULT_TIMEOUT = 3600
     RATELIMIT_DEFAULT = "100 per minute"
